3ff8a98b32e43d814a38d4d9dd5995aefec94149
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-23 17:36:31 -0300

    feat: add hash by bcryptjs and impl on create User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await hash(createUserDto.password, 10);
+
     const userCreated = this.prisma.user.create({
-      data: createUserDto })
+      data: { ...createUserDto, password: hashedPassword },
+    });
     return userCreated;
   }
 
