185e53bbe3d632640c881c0a3ec3cbccbc61caa0
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-23 14:47:17 -0300

    feat: inject PrismaService and impl create user method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const userCreated = this.prisma.user.create({
+      data: createUserDto })
+    return userCreated;
   }
 
   findAll() {
