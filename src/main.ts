3fa42ba18ffd4d6df0adddae877297496db69ef7
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-24 00:22:27 -0300

    feat: add swagger and global validation pipe configures

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // configure swagger
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Items API')
+    .setDescription('API para o meu CRUD de itens')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  // configure global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      validationError: { target: false },
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
