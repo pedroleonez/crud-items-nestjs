c794e2a44f938aff1f2d54447022000e898f82a2
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-24 15:09:57 -0300

    feat: add @ApiBody from swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,11 +2,16 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Create a new user',
+  })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
