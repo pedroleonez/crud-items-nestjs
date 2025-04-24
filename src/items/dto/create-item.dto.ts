3b0a4e247911cb1a725fe6e1eac3ef8eeaf8c288
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-24 15:10:15 -0300

    feat: impl CreateItemDto

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,38 @@
-export class CreateItemDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsInt, IsPositive } from 'class-validator';
+import { ValidationMessages } from '../../common/constants/validation-messages.constants';
+
+export class CreateItemDto {
+    @ApiProperty({
+        description: 'Item name',
+        example: 'Dell XPS Laptop',
+    })
+    @IsNotEmpty({ message: ValidationMessages.NOT_EMPTY('name') })
+    @IsString({ message: ValidationMessages.IS_STRING('name') })
+    name: string;
+
+    @ApiProperty({
+        description: 'Item description',
+        example: 'Laptop with i7 processor, 16GB RAM and 512GB SSD',
+    })
+    @IsNotEmpty({ message: ValidationMessages.NOT_EMPTY('description') })
+    @IsString({ message: ValidationMessages.IS_STRING('description') })
+    description: string;
+
+    @ApiProperty({
+        description: 'Item image URL',
+        example: 'https://example.com/laptop-image.jpg',
+    })
+    @IsNotEmpty({ message: ValidationMessages.NOT_EMPTY('image') })
+    @IsString({ message: ValidationMessages.IS_STRING('image') })
+    image: string;
+
+    @ApiProperty({
+        description: 'ID of the author/creator of the item',
+        example: 1,
+    })
+    @IsNotEmpty({ message: ValidationMessages.NOT_EMPTY('authorId') })
+    @IsInt({ message: ValidationMessages.IS_INT('authorId') })
+    @IsPositive({ message: ValidationMessages.IS_POSITIVE('authorId') })
+    authorId: number;
+}
